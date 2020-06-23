32c359b7a9a5ccde6b665d4f9b81b3471f4eda8d
AndresSalinas97 <i52salia@uco.es>
2020-06-23 13:43:40 +0200

    Terminar vista

--- src/valorador_view.py
+++ src/valorador_view.py
@@ -21,20 +21,20 @@ class ValoradorView():
     ValoradorMainWindow y permite acceder a ellas a partir de sus atributos.
 
     Atributos/Propiedades:
-        widget: QWidget con la interfaz del valorador (objeto de la clase
+        main_widget: QWidget con la interfaz del valorador (objeto de la clase
             ValoradorWidget).
-        mainWindow: Ventana principal (objeto de la clase ValoradorMainWindow).
+        main_window: Ventana principal (objeto de la clase ValoradorMainWindow).
     """
 
     def __init__(self):
-        self.widget = ValoradorWidget()
-        self.mainWindow = ValoradorMainWindow(self.widget)
+        self.main_widget = ValoradorWidget()
+        self.main_window = ValoradorMainWindow(self.main_widget)
 
     def show(self):
         """
         Hace visible la ventana principal.
         """
-        self.mainWindow.show()
+        self.main_window.show()
 
 
 class ValoradorWidget(QtGui.QWidget):
@@ -42,14 +42,33 @@ class ValoradorWidget(QtGui.QWidget):
     QWidget con la interfaz del valorador.
 
     Atributos/Propiedades:
-        TODO: Documentar
+        abrir_Button: QPushButton para abrir fichero del caso.
+        valorar_Button: QPushButton para valorar el caso.
+        reset_Button: QPushButton para reinicializar el caso.
+        ruta_caso_LineEdit: QLineEdit de solo lectura que muestra la ruta del
+                            fichero del caso.
+        valor_int_LineEdit: QLineEdit para introducir el valor de un criterio
+                            del tipo Entero.
+        valor_double_LineEdit: QLineEdit para introducir el valor de un criterio
+                               del tipo Porcentaje.
+        valor_ComboBox: QComboBox para introducir el valor de un criterio
+                        del tipo Booleano.
+        desc_caso_TextEdit: QTextEdit de solo lectura que muestra la
+                            descripción del caso.
+        desc_criterio_TextEdit: QTextEdit de solo lectura que muestra la
+                                descricpión del criterio seleccionado.
+        valoracion_LineEdit: QLineEdit de solo lectura que muestra el resultado
+                             de la valoración.
+        explicacion_TextEdit: QTextEdit de solo lectura que muestra la
+                              explicación de la valoración.
+        criterios_List: QListWidget con la lista de criterios del caso.
     """
 
     # Espacio vertical entre los elementos del grid layout
     _VERTICAL_SPACING = 10
 
     # Espacio horizontal entre los elementos del grid layout
-    _HORIZONTAL_SPACING = 30
+    _HORIZONTAL_SPACING = 20
 
     # Ancho mínimo de las columnas del grid layout
     _COLUMN_0_MIN_WIDTH = 200
@@ -57,9 +76,9 @@ class ValoradorWidget(QtGui.QWidget):
     _COLUMN_2_MIN_WIDTH = 250
 
     # Factor de estiramiento de las columnas del grid layout
-    _COLUMN_0_STRETCH = 1
-    _COLUMN_1_STRETCH = 2
-    _COLUMN_2_STRETCH = 2
+    _COLUMN_0_STRETCH = 2
+    _COLUMN_1_STRETCH = 3
+    _COLUMN_2_STRETCH = 3
 
     # Altura mínima de las final del grid layout
     _ROW_1_MIN_HEIGHT = 100
@@ -75,37 +94,42 @@ class ValoradorWidget(QtGui.QWidget):
         Inicialización de la interfaz.
         """
         ##### Fuentes #####
-        self.bold_label_font = QtGui.QFont()
-        self.bold_label_font.setBold(True)
+        bold_font = QtGui.QFont()
+        bold_font.setBold(True)
+
+        big_bold_font = QtGui.QFont()
+        big_bold_font.setBold(True)
+        big_bold_font.setPointSize(40)
 
         ##### Etiquetas #####
         caso_label = QtGui.QLabel(u"Caso")
-        caso_label.setFont(self.bold_label_font)
+        caso_label.setFont(bold_font)
 
-        desc_caso_label = QtGui.QLabel(u"Descripción del caso")
-        desc_caso_label.setFont(self.bold_label_font)
+        desc_caso_label = QtGui.QLabel(u"Descripción del Caso")
+        desc_caso_label.setFont(bold_font)
 
-        criterio_label = QtGui.QLabel(u"Criterio")
-        criterio_label.setFont(self.bold_label_font)
+        criterio_label = QtGui.QLabel(u"Criterios")
+        criterio_label.setFont(bold_font)
 
-        desc_criterio_label = QtGui.QLabel(u"Descripción del criterio")
-        desc_criterio_label.setFont(self.bold_label_font)
+        desc_criterio_label = QtGui.QLabel(u"Descripción del Criterio")
+        desc_criterio_label.setFont(bold_font)
 
-        valor_criterio_label = QtGui.QLabel(u"Valor")
-        valor_criterio_label.setFont(self.bold_label_font)
+        valor_criterio_label = QtGui.QLabel(u"Valor del Criterio")
+        valor_criterio_label.setFont(bold_font)
 
         valoracion_label = QtGui.QLabel(u"Valoración")
-        valoracion_label.setFont(self.bold_label_font)
+        valoracion_label.setFont(bold_font)
 
         explicacion_label = QtGui.QLabel(u"Explicación")
-        explicacion_label.setFont(self.bold_label_font)
+        explicacion_label.setFont(bold_font)
 
         ##### Botones #####
-        self.abrir_Button = QtGui.QPushButton(u"Abrir fichero de caso")
+        self.abrir_Button = QtGui.QPushButton(u"Abrir Caso")
         self.abrir_Button.setStatusTip(
             u"Abrir fichero JSON con el caso a valorar")
 
         self.valorar_Button = QtGui.QPushButton(u"VALORAR")
+        self.valorar_Button.setFont(bold_font)
         self.valorar_Button.setStatusTip(
             u"Iniciar proceso de valoración")
 
@@ -119,6 +143,35 @@ class ValoradorWidget(QtGui.QWidget):
         self.ruta_caso_LineEdit.setAlignment(QtCore.Qt.AlignCenter)
         self.ruta_caso_LineEdit.setStatusTip(u"Ruta del caso abierto")
 
+        self.valoracion_LineEdit = QtGui.QLineEdit()
+        self.valoracion_LineEdit.setReadOnly(True)
+        self.valoracion_LineEdit.setMinimumHeight(1)
+        self.valoracion_LineEdit.setFont(big_bold_font)
+        self.valoracion_LineEdit.setAlignment(QtCore.Qt.AlignCenter)
+        self.valoracion_LineEdit.setStatusTip(u"Resultado de la valoración")
+
+        ##### Campos para los distintos tipos del valor del criterio #####
+        self.valor_int_LineEdit = QtGui.QLineEdit()
+        self.valor_int_LineEdit.setValidator(QtGui.QIntValidator())
+        self.valor_int_LineEdit.setStatusTip(
+            u"Valor del criterio seleccionado (número entero)")
+
+        self.valor_double_LineEdit = QtGui.QLineEdit()
+        self.valor_double_LineEdit.setValidator(
+            QtGui.QDoubleValidator(0, 1, 4, self.valor_double_LineEdit))
+        self.valor_double_LineEdit.setVisible(False)
+        self.valor_double_LineEdit.setStatusTip(
+            u"Valor del criterio seleccionado " +
+            u"(porcentaje expresado con un número decimal entre 0.0 y 1.0)")
+
+        self.valor_ComboBox = QtGui.QComboBox()
+        self.valor_ComboBox.addItem(u"None")
+        self.valor_ComboBox.addItem(u"True")
+        self.valor_ComboBox.addItem(u"False")
+        self.valor_ComboBox.setVisible(False)
+        self.valor_ComboBox.setStatusTip(
+            u"Valor del criterio seleccionado (True o False)")
+
         ##### Campos de texto #####
         self.desc_caso_TextEdit = QtGui.QTextEdit()
         self.desc_caso_TextEdit.setReadOnly(True)
@@ -130,11 +183,6 @@ class ValoradorWidget(QtGui.QWidget):
         self.desc_criterio_TextEdit.setMinimumHeight(1)
         self.desc_criterio_TextEdit.setStatusTip(
             u"Descripción del criterio seleccionado")
-
-        self.valoracion_TextEdit = QtGui.QTextEdit()
-        self.valoracion_TextEdit.setReadOnly(True)
-        self.valoracion_TextEdit.setMinimumHeight(1)
-        self.valoracion_TextEdit.setStatusTip(u"Resultado de la valoración")
 
         self.explicacion_TextEdit = QtGui.QTextEdit()
         self.explicacion_TextEdit.setReadOnly(True)
@@ -151,8 +199,47 @@ class ValoradorWidget(QtGui.QWidget):
         self.criterios_List.setVerticalScrollBarPolicy(
             QtCore.Qt.ScrollBarAlwaysOn)
         self.criterios_List.setStatusTip(
-            "Criterios del caso (seleccione uno para examinarlo e introducir " +
-            "su valor)")
+            u"Criterios del caso (seleccione uno para examinarlo e " +
+            u"introducir su valor)")
+
+        ##### Cajas de layout #####
+        abrir_Layout = QtGui.QHBoxLayout()
+        abrir_Layout.setAlignment(QtCore.Qt.AlignCenter)
+        abrir_Layout.addWidget(self.abrir_Button)
+        abrir_Layout_Widget = QtGui.QWidget()
+        abrir_Layout_Widget.setLayout(abrir_Layout)
+
+        valorar_Layout = QtGui.QHBoxLayout()
+        valorar_Layout.setAlignment(QtCore.Qt.AlignCenter)
+        valorar_Layout.addWidget(self.valorar_Button)
+        valorar_Layout_Widget = QtGui.QWidget()
+        valorar_Layout_Widget.setLayout(valorar_Layout)
+
+        reset_Layout = QtGui.QHBoxLayout()
+        reset_Layout.setAlignment(QtCore.Qt.AlignCenter)
+        reset_Layout.addWidget(self.reset_Button)
+        reset_Layout_Widget = QtGui.QWidget()
+        reset_Layout_Widget.setLayout(reset_Layout)
+
+        ruta_caso_Layout = QtGui.QVBoxLayout()
+        ruta_caso_Layout.setAlignment(QtCore.Qt.AlignTop)
+        ruta_caso_Layout.addWidget(self.ruta_caso_LineEdit)
+        ruta_caso_Layout_Widget = QtGui.QWidget()
+        ruta_caso_Layout_Widget.setLayout(ruta_caso_Layout)
+
+        valoracion_Layout = QtGui.QVBoxLayout()
+        valoracion_Layout.setAlignment(QtCore.Qt.AlignTop)
+        valoracion_Layout.addWidget(self.valoracion_LineEdit)
+        valoracion_caso_Layout_Widget = QtGui.QWidget()
+        valoracion_caso_Layout_Widget.setLayout(valoracion_Layout)
+
+        valor_Layout = QtGui.QVBoxLayout()
+        valor_Layout.setAlignment(QtCore.Qt.AlignTop)
+        valor_Layout.addWidget(self.valor_int_LineEdit)
+        valor_Layout.addWidget(self.valor_double_LineEdit)
+        valor_Layout.addWidget(self.valor_ComboBox)
+        valor_Layout_Widget = QtGui.QWidget()
+        valor_Layout_Widget.setLayout(valor_Layout)
 
         ##### Configuración grid layout #####
         grid = QtGui.QGridLayout()
@@ -175,10 +262,10 @@ class ValoradorWidget(QtGui.QWidget):
         grid.addWidget(caso_label, 0, 0, 1, 2)
         grid.addWidget(desc_caso_label, 0, 2, 1, 1)
 
-        grid.addWidget(self.ruta_caso_LineEdit, 1, 0, 1, 2)
+        grid.addWidget(ruta_caso_Layout_Widget, 1, 0, 1, 2)
         grid.addWidget(self.desc_caso_TextEdit, 1, 2, 1, 1)
 
-        grid.addWidget(self.abrir_Button, 2, 0, 1, 3)
+        grid.addWidget(abrir_Layout_Widget, 2, 0, 1, 3)
 
         grid.addWidget(criterio_label, 3, 0, 1, 1)
         grid.addWidget(desc_criterio_label, 3, 1, 1, 1)
@@ -186,17 +273,17 @@ class ValoradorWidget(QtGui.QWidget):
 
         grid.addWidget(self.criterios_List, 4, 0, 1, 1)
         grid.addWidget(self.desc_criterio_TextEdit, 4, 1, 1, 1)
-        # grid.addWidget(self., 4, 2, 1, 1)
+        grid.addWidget(valor_Layout_Widget, 4, 2, 1, 1)
 
-        grid.addWidget(self.valorar_Button, 5, 0, 1, 3)
+        grid.addWidget(valorar_Layout_Widget, 5, 0, 1, 3)
 
         grid.addWidget(valoracion_label, 6, 0, 1, 2)
         grid.addWidget(explicacion_label, 6, 2, 1, 1)
 
-        grid.addWidget(self.valoracion_TextEdit, 7, 0, 1, 2)
+        grid.addWidget(valoracion_caso_Layout_Widget, 7, 0, 1, 2)
         grid.addWidget(self.explicacion_TextEdit, 7, 2, 1, 1)
 
-        grid.addWidget(self.reset_Button, 8, 0, 1, 3)
+        grid.addWidget(reset_Layout_Widget, 8, 0, 1, 3)
 
         self.setLayout(grid)
 
@@ -205,24 +292,24 @@ class ValoradorMainWindow(QtGui.QMainWindow):
     """
     Ventana principal del programa.
 
-    Contiene la barra de menús, la barra de herramientas, la barra de estado, y,
-    por supuesto, el widget con el valorador.
+    Contiene la barra de menús, la barra de estado, y, por supuesto, el widget
+    con el valorador.
 
-    Argumentos:
-        valoradorWidget: Widget con el valorador (objeto de la clase
+    Argumentos constructor:
+        valorador_Widget: Widget con el valorador (objeto de la clase
             valoradorWidget)
 
     Atributos/Propiedades:
-        valoradorWidget: Widget con el valorador (objeto de la clase
+        valorador_Widget: Widget con el valorador (objeto de la clase
             valoradorWidget)
-        exitAction: QAction para salir del programa.
-        openFileAction: QAction para abrir fichero de dominio.
+        exit_Action: QAction para salir del programa.
+        open_file_Action: QAction para abrir fichero de dominio.
     """
 
-    def __init__(self, valoradorWidget):
+    def __init__(self, valorador_Widget):
         super(ValoradorMainWindow, self).__init__()
 
-        self.valoradorWidget = valoradorWidget
+        self.valorador_Widget = valorador_Widget
 
         self._initUI()
 
@@ -232,30 +319,32 @@ class ValoradorMainWindow(QtGui.QMainWindow):
         """
 
         ##### Acciones #####
-        self.exitAction = QtGui.QAction("Salir", self)
-        self.exitAction.setShortcut('Ctrl+Q')
-        self.exitAction.setStatusTip("Salir del programa")
+        self.exit_Action = QtGui.QAction(u"Salir", self)
+        self.exit_Action.setShortcut('Ctrl+Q')
+        self.exit_Action.setStatusTip(u"Salir del programa")
 
-        self.openFileAction = QtGui.QAction("Abrir Fichero de dominio", self)
-        self.openFileAction.setShortcut('Ctrl+O')
-        self.openFileAction.setStatusTip("Abrir fichero")
+        self.open_file_Action = QtGui.QAction(
+            u"Abrir Fichero JSON de Caso", self)
+        self.open_file_Action.setShortcut('Ctrl+O')
+        self.open_file_Action.setStatusTip(
+            u"Abrir fichero JSON con el caso a valorar")
 
         ##### Barra de menús #####
-        menuBar = self.menuBar()
-        fileMenu = menuBar.addMenu('&Archivo')
-        fileMenu.addAction(self.openFileAction)
-        fileMenu.addSeparator()
-        fileMenu.addAction(self.exitAction)
+        menu_bar = self.menuBar()
+        file_Menu = menu_bar.addMenu(u"Archivo")
+        file_Menu.addAction(self.open_file_Action)
+        file_Menu.addSeparator()
+        file_Menu.addAction(self.exit_Action)
 
         ##### Barra de estado #####
         self.statusBar()  # Activa la barra de estado.
 
         ##### Widget contador #####
         # Añade a la ventana principal el contador.
-        self.setCentralWidget(self.valoradorWidget)
+        self.setCentralWidget(self.valorador_Widget)
 
         ##### Propiedades ventana #####
-        self.setWindowTitle("Valorador")
+        self.setWindowTitle(u"Valorador")
 
 
 class MessageBoxes():
@@ -271,7 +360,7 @@ class MessageBoxes():
         el argumento errorText.
         """
         msg = QtGui.QMessageBox()
-        msg.setWindowTitle("Error")
+        msg.setWindowTitle(u"Error")
         msg.setIcon(QtGui.QMessageBox.Critical)
         msg.setText(errorText)
         msg.exec_()
@@ -281,9 +370,11 @@ class MessageBoxes():
         """
         Muestra una ventana emergente de información con el mensaje indicado en
         el argumento infoText.
+
+        TODO: Eliminar método si no lo uso
         """
         msg = QtGui.QMessageBox()
-        msg.setWindowTitle("Informacion")
+        msg.setWindowTitle(u"Información")
         msg.setIcon(QtGui.QMessageBox.Information)
         msg.setText(infoText)
         msg.exec_()
@@ -296,9 +387,11 @@ class MessageBoxes():
 
         Devuelve:
             True si el usuario hace click en Ok; False en caso contrario.
+
+        TODO: Eliminar método si no lo uso
         """
         msg = QtGui.QMessageBox()
-        msg.setWindowTitle("Advertencia")
+        msg.setWindowTitle(u"Advertencia")
         msg.setIcon(QtGui.QMessageBox.Warning)
         msg.setText(infoText)
         msg.setStandardButtons(QtGui.QMessageBox.Ok | QtGui.QMessageBox.Cancel)
@@ -319,36 +412,10 @@ class MessageBoxes():
 
         Devuelve:
             String con la ruta del fichero seleccionado.
+
+        TODO: Modificar para que permita especificar la extensión del archivo a abrir
         """
-        return QtGui.QFileDialog.getOpenFileName(parent, "Abrir fichero")
-
-    @staticmethod
-    def openFolderDialog(parent):
-        """
-        Muestra una ventana de diálogo para seleccionar la carpeta a abrir.
-
-        Argumentos:
-            parent: QWidget padre.
-
-        Devuelve:
-            String con la ruta de la carpeta seleccionada.
-        """
-        return QtGui.QFileDialog.getExistingDirectory(
-            parent, "Seleccionar carpeta")
-
-    @staticmethod
-    def saveFileDialog(parent):
-        """
-        Muestra una ventana de diálogo para seleccionar dónde se guardará el
-        fichero.
-
-        Argumentos:
-            parent: QWidget padre.
-
-        Devuelve:
-            String con la ruta del fichero seleccionado.
-        """
-        return QtGui.QFileDialog.getSaveFileName(parent, 'Guardar Como...')
+        return QtGui.QFileDialog.getOpenFileName(parent, u"Abrir fichero")
 
 
 if __name__ == "__main__":
